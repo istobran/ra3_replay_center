a23246ec5d4e5b16d43df1fb06900dc5a2631ae6
BangZ <i@bangz.me>
2018-08-06 17:05:51 +0800

    added create directory automatically

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	_ "ra3_replay_center/routers"
 	"ra3_replay_center/utils"
 
@@ -9,7 +10,18 @@ import (
 	"github.com/astaxie/beego/logs"
 )
 
+const (
+	LOGPATH = "logs/"
+	REPPATH = "replays/"
+)
+
 func init() {
+	if _, err := os.Stat(LOGPATH); os.IsNotExist(err) {
+		os.Mkdir(LOGPATH, os.ModePerm)
+	}
+	if _, err := os.Stat(REPPATH); os.IsNotExist(err) {
+		os.Mkdir(LOGPATH, os.ModePerm)
+	}
 	logs.SetLogger(logs.AdapterFile, `{
 		"filename": "logs/replay_center_api.log",
 		"separate": ["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"],
@@ -17,19 +29,19 @@ func init() {
 		"maxdays": 30,
 		"rotate": true
 	}`)
-	mailsource, err := json.Marshal(map[string]interface{}{
-		"username":    beego.AppConfig.String("mail_username"),
-		"password":    beego.AppConfig.String("mail_password"),
-		"fromAddress": beego.AppConfig.String("mail_username"),
-		"host":        beego.AppConfig.String("mail_host"),
-		"sendTos":     []string{beego.AppConfig.String("mail_sendtos")},
-		"subject":     "RA3 Replay Center Error Report",
-		"level":       logs.LevelError,
-	})
-	if err != nil {
-		logs.Error(err)
-	}
-	logs.SetLogger(logs.AdapterMail, string(mailsource))
+	// mailsource, err := json.Marshal(map[string]interface{}{
+	// 	"username":    beego.AppConfig.String("mail_username"),
+	// 	"password":    beego.AppConfig.String("mail_password"),
+	// 	"fromAddress": beego.AppConfig.String("mail_username"),
+	// 	"host":        beego.AppConfig.String("mail_host"),
+	// 	"sendTos":     []string{beego.AppConfig.String("mail_sendtos")},
+	// 	"subject":     "RA3 Replay Center Error Report",
+	// 	"level":       logs.LevelError,
+	// })
+	// if err != nil {
+	// 	logs.Error(err)
+	// }
+	// logs.SetLogger(logs.AdapterMail, string(mailsource))
 	qcConfig, err := json.Marshal(map[string]interface{}{
 		"policyId":  beego.AppConfig.String("policy_id"),
 		"region":    beego.AppConfig.String("region"),
